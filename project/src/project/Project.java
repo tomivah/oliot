84cb64b6a72f0d95a91cb7e413c062a066291a4e
Teemu <Teemu@TeemunPC>
2017-04-22 15:54:01 +0300

    Testi

--- project/src/project/Project.java
+++ project/src/project/Project.java
@@ -13,7 +13,7 @@ public class Project {
                 default:
                     TextInterface.printLine( "Invalid selection" );
             }
-			System.out.println("Testiä vain");
+			System.out.println("Juupajuu");
         }
     }
 }
