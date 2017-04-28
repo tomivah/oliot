de2f5ca0a4fe078bb129a92be3d1efc54db01829
Teemu <Teemu@TeemunPC>
2017-04-28 09:06:01 +0300

    Friday commit

--- project/src/project/Store.java
+++ project/src/project/Store.java
@@ -10,6 +10,7 @@ public class Store {
 	private ArrayList<Customer> customerLine;
 	private ArrayList<Order> orders;
 	private ArrayList<Employee> freeEmployees;
+	private ArrayList<Customer> waitingCustomers;
 
 	public Store(double reputation, double money) {
 		this.reputation = reputation;
@@ -18,6 +19,8 @@ public class Store {
 		this.customerLine = new ArrayList();
 		this.orders = new ArrayList();
 		this.freeEmployees = new ArrayList();
+		// These are the customer that have ordered and are now waiting 
+		this.waitingCustomers = new ArrayList(); 
 
 		// Create a couple of employees right away?
 		createStartingEmployees();
@@ -27,14 +30,20 @@ public class Store {
 		Employee emp1 = new Employee();
 		Employee emp2 = new Employee();
 		Employee emp3 = new Employee();
+		Employee emp4 = new Employee();
+		Employee emp5 = new Employee();
 
 		this.employees.add(emp1);
 		this.employees.add(emp2);
 		this.employees.add(emp3);
+		this.employees.add(emp4);
+		this.employees.add(emp5);
 
 		this.freeEmployees.add(emp1);
 		this.freeEmployees.add(emp2);
 		this.freeEmployees.add(emp3);
+		this.freeEmployees.add(emp4);
+		this.freeEmployees.add(emp5);
 
 	}
 
@@ -50,7 +59,7 @@ public class Store {
 		this.employees.remove(emp);
 	}
 
-	public void addToLine(Customer customer) {
+	public void addCustomerToLine(Customer customer) {
 		this.customerLine.add(customer);
 	}
 
@@ -63,9 +72,7 @@ public class Store {
 	}
 
 	public Employee getFreeEmployee() {
-		Employee emp = this.freeEmployees.get(0);
-		this.freeEmployees.remove(emp);
-		return emp;
+		return this.freeEmployees.get(0);
 	}
 
 	public boolean customersInLine() {
@@ -73,12 +80,68 @@ public class Store {
 	}
 
 	public Customer getFirstCustomer() {
-		Customer cus = this.customerLine.get(0);
-		this.customerLine.remove(0);
-		return cus;
+		return this.customerLine.get(0);
+	}
+
+	public void removeCustomerFromLine(Customer customer) {
+		this.customerLine.remove(customer);
+	}
+
+	public void putCustomerToWaitingList(Customer customer) {
+		this.waitingCustomers.add(customer);
 	}
 
 	public void addOrder(Order order) {
 		this.orders.add(order);
+	}
+
+	public Iterable<Order> getOrders() {
+		return this.orders;
+	}
+
+	public void setEmployeeFree(Employee emp) {
+		this.freeEmployees.add(emp);
+	}
+
+	public void setEmployeeNotFree(Employee emp) {
+		this.freeEmployees.remove(emp);
+	}
+
+	public void removeCustomerFromWaitingList(Customer customer) {
+		this.waitingCustomers.remove(customer);
+	}
+
+	public Iterable<Customer> getCustomers() {
+		// This is unused and redundant
+		return this.customerLine;
+	}
+
+	public void removeOrder(Order order) {
+		this.orders.remove(order);
+	}
+
+	public void allCustomersWait() {
+		// How should the minutes affect patience?
+		for (Customer customer : this.customerLine) {
+			customer.decreasePatience();
+			customer.addLineMinute();
+		}
+
+		for (Customer customer : this.waitingCustomers) {
+			customer.decreasePatience();
+			customer.addWaitingMinute();
+		}
+	}
+
+	public void clearStore() {
+		// Throw out all customers, all orders and free all employees
+		this.customerLine.clear();
+		this.waitingCustomers.clear();
+		this.orders.clear();
+		for (Employee emp : this.employees) {
+			if (!this.freeEmployees.contains(emp)) {
+				this.freeEmployees.add(emp);
+			}
+		}
 	}
 }
